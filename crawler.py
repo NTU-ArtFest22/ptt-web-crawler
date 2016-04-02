782fd787818762474bc156892f4a1cb843af2f4b
how2945ard <how2945ard@gmail.com>
2016-04-02 19:14:42 +0800

    for joke

--- crawler.py
+++ crawler.py
@@ -32,16 +32,19 @@ def crawler(cmdline=None):
         Input: board name and page indices (or articla ID)
         Output: BOARD_NAME-START_INDEX-END_INDEX.json (or BOARD_NAME-ID.json)
     ''')
-    parser.add_argument('-b', metavar='BOARD_NAME', help='Board name', required=True)
+    parser.add_argument('-b', metavar='BOARD_NAME',
+                        help='Board name', required=True)
     group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument('-i', metavar=('START_INDEX', 'END_INDEX'), type=int, nargs=2, help="Start and end index")
+    group.add_argument('-i', metavar=('START_INDEX', 'END_INDEX'),
+                       type=int, nargs=2, help="Start and end index")
     group.add_argument('-a', metavar='ARTICLE_ID', help="Article ID")
-    parser.add_argument('-v', '--version', action='version', version='%(prog)s ' + __version__)
+    parser.add_argument('-v', '--version', action='version',
+                        version='%(prog)s ' + __version__)
 
     if cmdline:
         args = parser.parse_args(cmdline)
     else:
-       args = parser.parse_args()
+        args = parser.parse_args()
     board = args.b
     PTT_URL = 'https://www.ptt.cc'
     if args.i:
@@ -53,11 +56,12 @@ def crawler(cmdline=None):
         index = start
         filename = board + '-' + str(start) + '-' + str(end) + '.json'
         store(filename, u'{"articles": [\n', 'w')
-        for i in range(end-start+1):
+        for i in range(end - start + 1):
             index = start + i
             print('Processing index:', str(index))
             resp = requests.get(
-                url=PTT_URL + '/bbs/' + board + '/index' + str(index) + '.html',
+                url=PTT_URL + '/bbs/' + board +
+                '/index' + str(index) + '.html',
                 cookies={'over18': '1'}, verify=VERIFY
             )
             if resp.status_code != 200:
@@ -67,14 +71,18 @@ def crawler(cmdline=None):
             divs = soup.find_all("div", "r-ent")
             for div in divs:
                 try:
-                    # ex. link would be <a href="/bbs/PublicServan/M.1127742013.A.240.html">Re: [問題] 職等</a>
+                    # ex. link would be <a
+                    # href="/bbs/PublicServan/M.1127742013.A.240.html">Re: [問題]
+                    # 職等</a>
                     href = div.find('a')['href']
                     link = PTT_URL + href
                     article_id = re.sub('\.html', '', href.split('/')[-1])
-                    if div == divs[-1] and i == end-start:  # last div of last page
-                        store(filename, parse(link, article_id, board) + '\n', 'a')
+                    if div == divs[-1] and i == end - start:  # last div of last page
+                        store(filename, parse(
+                            link, article_id, board) + '\n', 'a')
                     else:
-                        store(filename, parse(link, article_id, board) + ',\n', 'a')
+                        store(filename, parse(
+                            link, article_id, board) + ',\n', 'a')
                 except:
                     pass
             time.sleep(0.1)
@@ -91,7 +99,7 @@ def parse(link, article_id, board):
     resp = requests.get(url=link, cookies={'over18': '1'}, verify=VERIFY)
     if resp.status_code != 200:
         print('invalid url:', resp.url)
-        return json.dumps({"error": "invalid url"}, indent=4, sort_keys=True, ensure_ascii=False)
+        return
     soup = BeautifulSoup(resp.text)
     main_content = soup.find(id="main-content")
     metas = main_content.select('div.article-metaline')
@@ -99,9 +107,12 @@ def parse(link, article_id, board):
     title = ''
     date = ''
     if metas:
-        author = metas[0].select('span.article-meta-value')[0].string if metas[0].select('span.article-meta-value')[0] else author
-        title = metas[1].select('span.article-meta-value')[0].string if metas[1].select('span.article-meta-value')[0] else title
-        date = metas[2].select('span.article-meta-value')[0].string if metas[2].select('span.article-meta-value')[0] else date
+        author = metas[0].select('span.article-meta-value')[0].string if metas[
+            0].select('span.article-meta-value')[0] else author
+        title = metas[1].select('span.article-meta-value')[0].string if metas[
+            1].select('span.article-meta-value')[0] else title
+        date = metas[2].select('span.article-meta-value')[0].string if metas[
+            2].select('span.article-meta-value')[0] else date
 
         # remove meta nodes
         for meta in metas:
@@ -122,13 +133,16 @@ def parse(link, article_id, board):
 
     # 移除 '※ 發信站:' (starts with u'\u203b'), '◆ From:' (starts with u'\u25c6'), 空行及多餘空白
     # 保留英數字, 中文及中文標點, 網址, 部分特殊符號
-    filtered = [ v for v in main_content.stripped_strings if v[0] not in [u'※', u'◆'] and v[:2] not in [u'--'] ]
-    expr = re.compile(u(r'[^\u4e00-\u9fa5\u3002\uff1b\uff0c\uff1a\u201c\u201d\uff08\uff09\u3001\uff1f\u300a\u300b\s\w:/-_.?~%()]'))
+    filtered = [v for v in main_content.stripped_strings if v[
+        0] not in [u'※', u'◆'] and v[:2] not in [u'--']]
+    expr = re.compile(
+        u(r'[^\u4e00-\u9fa5\u3002\uff1b\uff0c\uff1a\u201c\u201d\uff08\uff09\u3001\uff1f\u300a\u300b\s\w:/-_.?~%()]'))
     for i in range(len(filtered)):
         filtered[i] = re.sub(expr, '', filtered[i])
 
     filtered = [_f for _f in filtered if _f]  # remove empty strings
-    filtered = [x for x in filtered if article_id not in x]  # remove last line containing the url of the article
+    # remove last line containing the url of the article
+    filtered = [x for x in filtered if article_id not in x]
     content = ' '.join(filtered)
     content = re.sub(r'(\s)+', ' ', content)
     # print 'content', content
@@ -141,11 +155,15 @@ def parse(link, article_id, board):
             continue
         push_tag = push.find('span', 'push-tag').string.strip(' \t\n\r')
         push_userid = push.find('span', 'push-userid').string.strip(' \t\n\r')
-        # if find is None: find().strings -> list -> ' '.join; else the current way
+        # if find is None: find().strings -> list -> ' '.join; else the current
+        # way
         push_content = push.find('span', 'push-content').strings
-        push_content = ' '.join(push_content)[1:].strip(' \t\n\r')  # remove ':'
-        push_ipdatetime = push.find('span', 'push-ipdatetime').string.strip(' \t\n\r')
-        messages.append( {'push_tag': push_tag, 'push_userid': push_userid, 'push_content': push_content, 'push_ipdatetime': push_ipdatetime} )
+        push_content = ' '.join(push_content)[
+            1:].strip(' \t\n\r')  # remove ':'
+        push_ipdatetime = push.find(
+            'span', 'push-ipdatetime').string.strip(' \t\n\r')
+        messages.append({'push_tag': push_tag, 'push_userid': push_userid,
+                         'push_content': push_content, 'push_ipdatetime': push_ipdatetime})
         if push_tag == u'推':
             p += 1
         elif push_tag == u'噓':
@@ -154,30 +172,44 @@ def parse(link, article_id, board):
             n += 1
 
     # count: 推噓文相抵後的數量; all: 推文總數
-    message_count = {'all': p+b+n, 'count': p-b, 'push': p, 'boo': b, "neutral": n}
+    message_count = {'all': p + b + n, 'count': p -
+                     b, 'push': p, 'boo': b, "neutral": n}
 
+    exprTitle = re.compile(
+        u(r'(Re:)|(公告)|(\[妞告 \])|(\版\主)|(版主卸任)|(\Ψ公告\Ψ)'))
+    exprContent = re.compile(
+        u(r'(引述)|(之銘言)|(發信人)|(\版\主)'))
+
+    if p - b <= 0 or n > p or re.match(exprTitle, title) != None or re.match(exprContent, content) != None:
+        return
     # print 'msgs', messages
     # print 'mscounts', message_count
 
+    print(title)
+    print(re.match(exprTitle, title))
     # json data
     data = {
-        'board': board,
-        'article_id': article_id,
+        # 'board': board,
+        # 'article_id': article_id, 
         'article_title': title,
         'author': author,
         'date': date,
         'content': content,
-        'ip': ip,
+        # 'ip': ip,
         'message_conut': message_count,
-        'messages': messages
+        # 'messages': messages
     }
     # print 'original:', d
     return json.dumps(data, indent=4, sort_keys=True, ensure_ascii=False)
 
+
 def getLastPage(board):
-    content = urlopen('https://www.ptt.cc/bbs/' + board + '/index.html').read().decode('utf-8')
-    first_page = re.search(r'href="/bbs/' + board + '/index(\d+).html">&lsaquo;', content).group(1)
+    content = urlopen('https://www.ptt.cc/bbs/' + board +
+                      '/index.html').read().decode('utf-8')
+    first_page = re.search(r'href="/bbs/' + board +
+                           '/index(\d+).html">&lsaquo;', content).group(1)
     return int(first_page) + 1
+
 
 def store(filename, data, mode):
     with codecs.open(filename, mode, encoding='utf-8') as f:
